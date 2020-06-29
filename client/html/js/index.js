5cf6b66abe439f72f43d85703f53aa7c34a12733
viola <viola@beats.net.cn>
2020-06-29 18:04:51 +0800

    提交:除了气泡,其他逻辑已完成

--- client/html/js/index.js
+++ client/html/js/index.js
@@ -4,6 +4,15 @@ $(document).ready(function () {
   var articleBox = $('article');
   var windowScale = window.innerWidth / 750;
 
+  var _video = $('#video')[0];
+  var isHeartAni = false;
+  var soundList;
+
+  //解锁包包
+  var packageObj = {}
+  var ticketData = '',ticketSrc = '';
+  var _secretkey = 'dior-test';//上传到cdn的目录名称，上线后要改为正式地址
+
   //----------------------------------------有微信授权放这里  授权完在 icom.init(init)----------------------------------------
 
   icom.init(init); //初始化
@@ -13,9 +22,12 @@ $(document).ready(function () {
     requestAnimationFrame(function () {
       var screenProp = window.innerWidth / window.innerHeight;
       console.log('os.screenProp:' + os.screenProp);
-      if (screenProp < 0.54) articleBox.addClass('screen189');
-      if (screenProp > 0.64) articleBox.addClass('screen159');
+      if (os.screenProp < 0.54) articleBox.addClass('screen189');
+      if (os.screenProp > 0.54 && os.screenProp < 0.62)
+        articleBox.addClass('screenNormal');
+      if (os.screenProp >= 0.62) articleBox.addClass('screen159');
       load_handler();
+      sound_handler();
     });
   } //edn func
 
@@ -50,38 +62,187 @@ $(document).ready(function () {
     else setTimeout(load_timer, 33, per);
   } //edn func
 
+  //------------------音乐
+  function sound_handler() {
+    if (os.weixin) {
+        try {
+            WeixinJSBridge.invoke('getNetworkType', {},
+            sound_creat);
+        } catch(e) {
+            wx.ready(sound_creat);
+        }
+    } else sound_creat();
+  } // end func
+  var soundListArr = [{
+    src: 'https://upload.cdn.be-xx.com/cilabo/sound/soundTimer.mp3'
+  }];
+  function sound_creat() {
+    soundList = iaudio.on(soundListArr);
+  } // end func
+  
+
+
   //----------------------------------------页面逻辑代码----------------------------------------
 
   /**
    * 页面初始化
    */
-	function pageInit() {
+  function pageInit() {
+    timeUplate();
+    videoEnd();
 		eventInit();
     monitor_handler();
 	} //end func
 	
 	function eventInit() {
-		$('#btnPlay').on('click', btnPlayClick);
-	}
+    $('#btnPlay').on('click', btnPlayClick);
+    $('#btnPointer').on('click', btnPointerClick);
+    $('#owerHoneyedPage .close').on('click', function () {
+      $('#owerHoneyedPage').removeClass('active');
+      _video.play();
+    });
+    $('#sharePage .close').on('click', function () {
+      $('#sharePage').removeClass('active');
+    });
+    //即刻分享
+    $('.btnShare').on('click', btnShareClick);
+    //立即购买
+    $('.btnBuy').on('click', btnBuyClick);
+  }
+  
+  /**即刻分享 */
+  function btnShareClick() {
+    $('#sharePage').addClass('active');
+    setTimeout(image_combine, 1500);
+  }
+
+  /**立即购买 */
+  function btnBuyClick() {
+    icom.alert('敬请期待');
+  }
 
 	/**开始播放 */
-	function btnPlayClick() {
-		$(this).hide();
-		var _video = $('#video')[0];
-		// _video.currentTime = 0;
+  function btnPlayClick() {
+    setTimeout(function () {
+      $('#btnPlay').fadeOut();
+    },100)
 		_video.play();
 	}
 
-	/**检测视频播放时长 */
+  /**播放动效心 */
+  function btnPointerClick() {
+    isHeartAni = true;
+    $('.heartbox').css({ opacity: 1 }).children('.heartcont').addClass('heart_on');
+    setTimeout(function () {
+      $('#btnPointer').fadeOut();
+      // $('#barragePage').addClass('active');
+      initBarrageAni();
+    }, 2500);
+  }
+
+  /**检测视频播放时长 */
+  function timeUplate() {
+    _video.addEventListener("timeupdate",function(){
+      var timeDisplay;
+      //用秒数来显示当前播放进度
+      timeDisplay = Math.floor(_video.currentTime);
+      //当视频播放到 4s的时候做处理
+      if(timeDisplay == 4 && !isHeartAni){
+        //处理代码
+        $('#btnPointer').fadeIn();
+        _video.pause();
+      }
+    },false);   
+  }
+
+  /**检测视频播放完毕 */
+  function videoEnd() {
+    _video.addEventListener('ended', function () {
+      //结束
+      $('#buyPage').addClass('active');
+    }, false);
+  }
+
+  /**弹幕动画 */
+  function initBarrageAni() {
+    initOwerHoneyedPage();
+    return
+    var box = $('#barragePage .item');
+    TweenMax.staggerTo(box, 1, { y:100}, 0.5);
+  }
+
+  /**owerHoneyedPage */
+  function initOwerHoneyedPage() {
+    if (!window.localStorage.packageNum) {
+      var num = imath.randomRange(1, 6);
+      window.localStorage.packageNum = num;
+    } else {
+      var num = Number(window.localStorage.packageNum);
+    }
+    packageObj.title = 'images/package/p' + num + '_tit.png';
+    packageObj.txt = 'images/package/p' + num + '_txt.png';
+    packageObj.box = 'images/package/p' + num + '.png';
+    $('.p_tit').attr('src', packageObj.title);
+    $('.p_txt').attr('src', packageObj.txt);
+    $('.p_box').attr('src', packageObj.box);
+    if (num == 1) {
+      $('.p_box').parent('.boxCon').addClass('boxCon1');
+    } else {
+      $('.p_box').parent('.boxCon').removeClass('boxCon1');
+    }
+    $('#owerHoneyedPage').addClass('active');
+  }
 
 
+  // =======================合成图片
+  function image_combine() {
+    var imgContent = $('#imgCanvas');
+    html2canvas(imgContent[0], {
+      logging: false,
+      useCORS: true,
+      dpi: window.devicePixelRatio,
+      scale: 2,
+    }).then(canvas => {
+      var base = canvas.toDataURL('image/jpeg', 1);
+      var img = new Image();
+      img.onload = function() {
+        $('<img src="' + base + '">').appendTo($('#imgContent'));
+        console.log('合并完成');
+        loadBox.hide();
+        $('#imgContent').show();
+        ticketData = base;
+        //上传到cdn
+        // uolpadBase64();
+      };
+      img.src = base;
+    });
+  } // end func
 
+  // 上传到cdn
+  function uolpadBase64() {
+    base64_send(ticketData, image_combine_complete, _secretkey);
+  } // edn func
 
+  function base64_send(data, callback, secretkey) {
+    $.post(apirul, { data: data, key: secretkey }, function(resp) {
+      var resp = JSON.parse(resp);
+      if (resp.errcode == 0) {
+        callback(resp.result);
+      } // edn if
+      else {
+        console.log('errmsg:' + resp.errmsg);
+        icom.alert(resp.errmsg);
+        loadBox.hide();
+      } // edn else
+    });
+  } // end func
 
-
-
-
-
+  function image_combine_complete(src) {
+    ticketSrc = src;
+    console.log('ticketSrc:' + ticketSrc);
+    loadBox.hide();
+    //==上传到后台
+  } // end func
 
 
 
